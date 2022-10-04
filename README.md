32957dc38f6c388dc09d0d6316264e56d0d3e901
Andres <andrarias@gmail.com>
2022-10-04 10:05:15 -0300

    readme1

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # mensajeriaMEAN
-webapps  y una red social  con el MEAN Stack, Angular, NodeJS, MongoDB, Bootstrap, CSS3 y +
-Back-end con API RESTful con Node y Base de datos Mongo con inrterface mongoAtlas
-login y rgistro
-Paginacion
-Front-end con cliente Angular
-Publicacione y timeline con paginacion
-modulo de mensajes privados con lazyLoad, guards control de acceso
+* webapps  y una red social  con el MEAN Stack, Angular, NodeJS, MongoDB, Bootstrap, CSS3 y +
+* Back-end con API RESTful con Node y Base de datos Mongo con inrterface mongoAtlas
+* login y rgistro
+* Paginacion, Publicacione y timeline con paginacion
+* Front-end con cliente Angular.
+* modulo de mensajes privados con lazyLoad, guards control de acceso.
+* DDD- Agregamos Clean Architecture.
--- api/cleanArchitect/domain/task.mapeo.ts
+++ api/cleanArchitect/domain/task.mapeo.ts
@@ -1,5 +1,6 @@
 //const {ITask} = require ('./task.model');
 import {ITask} from './task.IEntity';
+import { Uuid } from './Uuid';
 const { v4: uuid } = require('uuid');
 
 
@@ -8,16 +9,17 @@ const { v4: uuid } = require('uuid');
  */
 export class TaskMapeo implements ITask {
 
-    uuid: string;
+    uuid: string;    
     name: string;
     añoInicio: number;
     description: string;
 
     constructor(name: string, añoInicio?: number, description?: string) { //puede ser opcional
-        this.uuid = uuid();
+       
+        this.uuid = new Uuid().generaUuid();
         this.name = name;
-        this.añoInicio = añoInicio??100;
-        this.description = description??"Default";
+        this.añoInicio = añoInicio ?? 100;  // opcion 100 igual que abajo
+        this.description = description || "Default"; //opcion default
     }
 
 
